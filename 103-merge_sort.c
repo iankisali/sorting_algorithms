ee524f3c34a4b4b4e89ad54dd4d7dc12122ce65b
iankisali <iankisali@gmail.com>
2022-11-06 22:54:26 -0800

    sorting integers in ascending using merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,72 +1,70 @@
 #include "sort.h"
 
 /**
- * merge - merging arrays
- * @array: array to merge
- * @start: start of array
- * @middle: array middle
- * @end: array end
+ * merge_subarr - sort subarray of int
+ * @subarr: subarray of array
+ * @buff: store sorted array
+ * @front: fornt index of array
+ * @mid: middle index of array
+ * @back: back index of array
  */
-void merge(int *array, int start, int middle, int end)
+void merge_subarr(int *subarr, int *buff, size_t front,
+		size_t mid, size_t back)
 {
-	int l1, l2, l3, l4 = 0;
-	int size1 = middle - start + 1, size2 = end - middle;
-	int temp1[4096], temp2[4096];
-	int *temp3 = malloc(sizeof(int) * (size1 + size2));
-
-	for (l1 = 0, l2 = 0; l1 < size1 || l2 < size2; l1++, l2++)
-	{
-		temp1[l1] = array[start + l1];
-		temp2[l2] = array[middle + 1 + l2];
-	}
-	for (l1 = l2 = 0, l3 = start; l1 < size1 && l2 < size2; l3++)
-		if (temp1[l1] <= temp2[l2])
-			array[l3] = temp3[l4++] = temp1[l1++];
-		else
-			array[l3] = temp3[l4++] = temp2[l2++];
+	size_t i, j, k = 0;
 
 	printf("Merging...\n[left]: ");
-	print_array(temp1, size1);
+	print_array(subarr + front, mid - front);
 	printf("[right]: ");
-	print_array(temp2, size2);
+	print_array(subarr + mid, back - mid);
 
-	while (l1 < size1)
-		array[l3++] = temp3[l4++] = temp1[l1++];
-	while (l2 < size2)
-		array[l3++] = temp3[l4++] = temp1[l2++];
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
 
 	printf("[Done]: ");
-	print_array(temp3, size1 + size2);
-	free(temp3);
+	print_array(subarr + front, back - front);
 }
 
 /**
- * sort - sort array and merge
- * @array: array to sort
- * @start: stary af arry
- * @end: array end
+ * merge_recursively - merging using recursion
+ * @subarr: subarray of array
+ * @buff: buffer to store sorted
+ * @front: front index of subarray
+ * @back: back index of subarray
  */
-void sort(int *array, int start, int end)
+void merge_recursively(int *subarr, int *buff, size_t front, size_t back)
 {
-	int middle;
+	size_t mid;
 
-	if (start < end)
+	if (back - front > 1)
 	{
-		middle = (start + end - 1) / 2;
-		sort(array, start, middle);
-		sort(array, middle + 1, end);
-		merge(array, start, middle, end);
+		mid = front + (back - front) / 2;
+		merge_recursively(subarr, buff, front, mid);
+		merge_recursively(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
 	}
 }
 
 /**
- * merge_sort - perfoming merger sort
- * @array: array of int to sort
- * @size: size af array
+ * merge_sort - sorting array of int using merger sort
+ * @array: array of int
+ * @size: size of array
  */
 void merge_sort(int *array, size_t size)
 {
+	int *buff;
+
 	if (array == NULL || size < 2)
 		return;
-	sort(array, 0, size - 1);
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+	merge_recursively(array, buff, 0, size);
+	free(buff);
 }
